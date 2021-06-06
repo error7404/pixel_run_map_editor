976b8003c82b535a6bf8c02e22715a57e986f7c4
error7404 <error7404@users.noreply.github.com>
2021-06-06 14:48:51 +0200

    bloc selector

--- lib/Pages/MapViewer.dart
+++ lib/Pages/MapViewer.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pixel_run_map_editor/Pages/SaveMap.dart';
 
+int _currentBloc = 0;
+
 class MapViewer extends StatefulWidget {
   final File file;
 
@@ -44,41 +46,73 @@ class _MapViewerState extends State<MapViewer> {
           )
         ],
       ),
-      body: Scrollbar(
-        child: InteractiveViewer(
-          scaleEnabled: false,
-          constrained: false,
-          child: Column(
-            children: List.generate(
-              map!.length + 1,
-              (int index) => Row(
-                children: List.generate(
-                  map![0].length + 1,
-                  (int index2) {
-                    try {
-                      return block(
-                        id: map![index][index2],
-                        map: map!,
-                        x: index,
-                        y: index2,
-                        callback: (newMap) => setState(() => map = newMap),
-                      );
-                    } catch (e) {
-                      // rendering no block when out of blocks
-                      return block(
-                        id: 1,
-                        map: map!,
-                        x: index,
-                        y: index2,
-                        callback: (newMap) => setState(() => map = newMap),
-                      );
-                    }
-                  },
-                ),
+      body: InteractiveViewer(
+        scaleEnabled: false,
+        constrained: false,
+        child: Column(
+          children: List.generate(
+            map!.length + 1,
+            (int index) => Row(
+              children: List.generate(
+                map![0].length + 1,
+                (int index2) {
+                  try {
+                    return block(
+                      id: map![index][index2],
+                      map: map!,
+                      x: index,
+                      y: index2,
+                      callback: (newMap) => setState(() => map = newMap),
+                    );
+                  } catch (e) {
+                    // rendering no block when out of blocks
+                    return block(
+                      id: 1,
+                      map: map!,
+                      x: index,
+                      y: index2,
+                      callback: (newMap) => setState(() => map = newMap),
+                    );
+                  }
+                },
               ),
             ),
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentBloc,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/Bloc_de_terre_2.png"),
+              label: "Bloc_de_terre"),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: 64,
+                height: 64,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: const Color(0xFF000000),
+                  ),
+                ),
+              ),
+              label: "Air"),
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/Porte.png"), label: "Porte"),
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/Pic.png"), label: "Pic")
+        ],
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+        fixedColor: Colors.green,
+        mouseCursor: SystemMouseCursors.click,
+        onTap: (value) {
+          setState(() {
+            _currentBloc = value;
+          });
+        },
       ),
     );
   }
@@ -119,26 +153,33 @@ class block extends StatelessWidget {
       default:
         block = "assets/Bloc_de_terre_2.png";
     }
-    return GestureDetector(
-      onTap: () {
-        try {
-          updateMap(map, x, y, id < 3 ? id + 1 : 0);
-          // print(map);
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        // onTap: () {
+        //   try {
+        //     updateMap(map, x, y, id < 3 ? id + 1 : 0);
+        //     // print(map);
+        //     callback(map);
+        //   } catch (e) {
+        //     print("pas dans le map $e");
+        //   }
+        // },
+        onTap: () {
+          updateMap(map, x, y, _currentBloc);
           callback(map);
-        } catch (e) {
-          print("pas dans le map $e");
-        }
-      },
-      child: block != ""
-          ? Image.asset(
-              block,
-              filterQuality: FilterQuality.none,
-            )
-          : SizedBox(
-              height: 64,
-              width: 64,
-              child: Text(""),
-            ),
+        },
+        child: block != ""
+            ? Image.asset(
+                block,
+                filterQuality: FilterQuality.none,
+              )
+            : SizedBox(
+                height: 64,
+                width: 64,
+                child: Text(""),
+              ),
+      ),
     );
   }
 }
